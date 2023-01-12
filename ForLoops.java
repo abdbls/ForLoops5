4a697cf1c57c75ae7c368a036d235cbd6b46ea11
abdbls <belasabderrahmane@gmail>
2023-01-13 00:20:58 +0100

    First Version

---
+++ ForLoops.java
@@ -0,0 +1,16 @@
+package Classs7;
+
+
+
+public class ForLoops {
+    public static void main(String[] args) {
+
+for (int i=1;i<20;i+=2){
+    System.out.println(i);
+}
+
+
+
+
+    }
+}
---
+++ ForLoops3.java
@@ -0,0 +1,14 @@
+package Classs7;
+public class ForLoops3 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+
+
+
+
+    }
+}
